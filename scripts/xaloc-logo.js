ce24081860a308ed095e585d430e55adb4f750f1
Josep Sànchez Arbona <josep.sanchez.arbona@gmail.com>
2026-03-02 14:57:19 +0100

    xaloc logo with lottie file

---
+++ scripts/xaloc-logo.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  lottie.loadAnimation({
+    container: document.getElementById('xaloc-logo'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'images/hero/xaloc_logo.json'
+  });
+});
